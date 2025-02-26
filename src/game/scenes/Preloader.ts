16ad11b5bab0e66df2969af1d987a2d72cb4952f
Matthew Hanlon <matt.k.hanlon@gmail.com>
2025-02-25 20:33:43 -0400

    Adding the start screen.

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -134,7 +134,7 @@ export class Preloader extends Phaser.Scene {
         //  For example, you can define global animations here, so we can use them in other scenes.
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start(SceneKeys.Game);
+        this.scene.start(SceneKeys.MainMenu);
     }
 }
 
